00b9157098deae9b2f0630e4e73c9037047281fd
dawgy <theflood@gmail.com>
2013-11-13 10:18:18 -0500

    oxi made me do this...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IdiomaticLogs::Application.config.secret_key_base = 'bbb3815bda21c4e256de07130752f023e63cf1eccd12344a7428429d73a1fa97899734953d3048af467abd595ff00e296e9052898df5b27d9626e88547441850'
